fee915abc9bef1bbe5aa3757af7de2a50937e6f0
chemicat53 <chemicat53@gmail.com>
2021-08-13 12:28:18 +0900

    UPDATED INSTALLING/RUNNING BAT

--- 1_INSTALL.bat
+++ 1_INSTALL.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 CLS
-curl -L https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe -O %~dp0\python-3.9.5-amd64.exe
+curl -L "https://www.python.org/ftp/python/3.9.5/python-3.9.5-amd64.exe" -O %~dp0\python-3.9.5-amd64.exe
 %~dp0\python-3.9.5-amd64.exe /quiet SimpleInstall=1 InstallAllUsers=1 PrependPath=1 Include_test=0 Include_launcher=0 Include_tcltk=0
 pause
 python -m venv venv
-%~dp0\venv\Scripts\pip.exe install xlrd openpyxl pandas
+pip3 install xlrd openpyxl pandas
 pause
--- tekken_dist/2_RUN.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-CLS
-cmd.exe /K %~dp0\venv\Scripts\python.exe tekken.py
-pause
